f8af02f1662b6c93745d82b9fb88e5d5253e8af4
kschuppius <kschuppius@gmail.com>
2023-10-30 19:50:19 -0400

    Revert "inclass- work"
    This reverts commit 38f2e5d484bc6db55a4e2b5f00c5d2d4463346d2.

--- src/PolarisChip.js
+++ src/PolarisChip.js
@@ -5,53 +5,25 @@ import { LitElement, html, css } from 'lit';
 export class PolarisChip extends LitElement {
   static get properties() {
     return {
-      name: { type: String },
-      link: {type: String},
-      active: { type: Boolean, reflect: true }
+      title: { type: String },
     };
   }
 
   static get styles() {
     return css`
       :host {
-        display: inline-block;
-        margin:0 12px 12px 0;
-      }
-      .link:hover,
-      :host([active]) .link { 
-        background-color : #e4e5e7;
-        border: 2px solid #e4e5ef;
-        border-radius: 2px;
-        color: #005fa9;
-        cursor: pointer;
-      }
-      .link{ 
-        font- weight: bold; 
-        text-decoration: none; 
-        padding: 8px 4px; 
-        bording: 2px solid #444;
-        color: #444
-        font-size: 16px;
-      }
-
-
-      span {
-        padding: 8px 4px;
-        border: 2px solid #444;
-        color: #444;
-        font-size: 16px;
+        display: block;
       }
     `;
   }
 
   constructor() {
     super();
-    this.name = 'Football';
-    this.link = "https://www.psu.edu/news/research";
+    this.title = 'My boilerplate';
   }
 
   render() {
-    return html`<a class= "link" href= "${this.link}"><slot>${this.name}</slot></a>`;
+    return html`<span>${this.title}</span>`;
   }
 }
 
